59d4c000c835452d270c2ca60caa0d75488f1554
Puneeth <160226241+puneethpalaparthi@users.noreply.github.com>
2025-02-24 15:47:57 +0530

    Create 6(b) doc.js

---
+++ week-5/6(b) doc.js
@@ -0,0 +1,14 @@
+function showalert(){
+    window.alert("Show");
+
+}
+function writ(){
+    document.write("hello");
+}
+function cons(){
+    console.log("Puneeth");
+
+}
+function update(){
+    document.getElementById("hi").innerHTML="Prabhas";
+}
