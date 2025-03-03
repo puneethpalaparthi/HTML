bbe1750cf4ad5d5a043321815509a4089e0a530e
Puneeth <160226241+puneethpalaparthi@users.noreply.github.com>
2025-03-03 15:13:03 +0530

    Update 8(a) doc.js

--- week-6/8(a) doc.js
+++ week-6/8(a) doc.js
@@ -3,13 +3,12 @@ function largest(){
     var b = prompt("enter the second number");
     var c = prompt("enter the third number");
     if(a>=b && a>=c){
-        alert("a is big");
+        alert(a+ " is big");
     }
     else if(b>=a && b>=c){
-        alert("b is big");
+        alert(b +" is big");
     }
     else{
-        alert("c is big");
+        alert(c +" is big");
     }
-    
-}   1
+}   
